bce3af15ee75d3309af37dd495afb70bcd16479a
Xuan0629 <xuan.zheng@ontariotechu.net>
2023-11-05 15:36:30 -0500

    friends list homepage update

--- student_helper_app/lib/pages/friends_list_home_page.dart
+++ student_helper_app/lib/pages/friends_list_home_page.dart
@@ -4,55 +4,190 @@
 // sharing on. *save this for later/not high priority
 //small button (in appbar?) to add friends - editing and deleting should be done in friend's profile
 
+// import 'package:flutter/material.dart';
+//
+// class FriendListPage extends StatefulWidget {
+//   const FriendListPage({super.key});
+//
+//   @override
+//   State<StatefulWidget> createState() => _FriendListPageState();
+// }
+//
+// class _FriendListPageState extends State<FriendListPage> {
+//
+//   //get list of friends from cloud storage?
+//   // var friends_list = ...
+//   final tempFriendList = ["friend 1", "friend 2", "friend 3", "friend 4"];  //temp example
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//           title: const Text("Friends List"),
+//         ),
+//         body: Column(
+//           children: <Widget>[
+//             SizedBox(height: 10,),
+//             //temp placement for map if we want to have the map idea
+//             Container(
+//               padding: EdgeInsets.all(10.0),
+//               width: 360,
+//               height: 250,
+//               color: Colors.red,
+//             ),
+//             SizedBox(height: 10,),
+//             Expanded(
+//                 child: ListView.separated(
+//                   //update this with the actual friends list from database
+//                     padding: EdgeInsets.all(10),
+//                     itemCount: tempFriendList.length,
+//                     separatorBuilder: (context, index) => Divider(height: 2),
+//                     itemBuilder: (context, index) {
+//                       final friend = tempFriendList[index];
+//                       return ListTile(
+//                         title: Text(friend),
+//                         tileColor: index % 2 == 0 ? Colors.indigoAccent : Colors.blue,
+//                       );
+//                     }
+//                     )
+//             )
+//           ],
+//         ));
+//   }
+// }
 import 'package:flutter/material.dart';
 
-class FriendListPage extends StatefulWidget {
-  const FriendListPage({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _FriendListPageState();
+// for test only
+void main() {
+  runApp(MyApp());
 }
 
-class _FriendListPageState extends State<FriendListPage> {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Friend List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FriendListPage(),
+    );
+  }
+}
 
-  //get list of friends from cloud storage?
-  // var friends_list = ...
-  final tempFriendList = ["friend 1", "friend 2", "friend 3", "friend 4"];  //temp example 
+class FriendListPage extends StatelessWidget {
+  final List<Map<String, dynamic>> friends = [
+    {
+      'name': 'Alice',
+      'status': 'Online',
+      'lastMessage': 'Hey there! How are you?',
+      'isLastMessageFromUser': false,
+    },
+    {
+      'name': 'Bob',
+      'status': 'Offline',
+      'lastMessage': 'See you tomorrow!',
+      'isLastMessageFromUser': true,
+    },
+    {
+      'name': 'Charlie',
+      'status': 'Busy',
+      'lastMessage': 'Can\'t talk now, sorry!',
+      'isLastMessageFromUser': false,
+    },
+  ];
+
+  String userStatus = 'Online';
+
+  Color getStatusColor(String status) {
+    switch (status) {
+      case 'Online':
+        return Colors.green;
+      case 'Busy':
+        return Colors.red;
+      case 'Offline':
+        return Colors.grey;
+      default:
+        return Colors.grey;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Friends List"),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle, size: 45),
+          onPressed: () {
+            // Navigate to user profile page
+          },
         ),
-        body: Column(
-          children: <Widget>[
-            SizedBox(height: 10,),
-            //temp placement for map if we want to have the map idea
-            Container(
-              padding: EdgeInsets.all(10.0),
-              width: 360,
-              height: 250,
-              color: Colors.red,
+        title: Text('Friend List'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // Navigate to Add Friend page
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.circle, color: getStatusColor(userStatus)),
+            onPressed: () {
+              // Change user status
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: friends.length,
+              itemBuilder: (context, index) {
+                var friend = friends[index];
+                return ListTile(
+                  leading: Icon(Icons.account_circle, size: 40),
+                  title: Text('${friend['name']} - ${friend['status']}'),
+                  subtitle: Text(
+                    '${friend['isLastMessageFromUser'] ? 'Me' : friend['name']}: ${friend['lastMessage']}',
+                  ),
+                  trailing: Icon(Icons.circle, color: getStatusColor(friend['status'])),
+                  onTap: () {
+                    // Navigate to Chat page with the friend
+                  },
+                );
+              },
             ),
-            SizedBox(height: 10,),
-            Expanded(
-                child: ListView.separated(
-                  //update this with the actual friends list from database
-                    padding: EdgeInsets.all(10),
-                    itemCount: tempFriendList.length,
-                    separatorBuilder: (context, index) => Divider(height: 2),
-                    itemBuilder: (context, index) {
-                      final friend = tempFriendList[index];
-                      return ListTile(
-                        title: Text(friend),
-                        tileColor: index % 2 == 0 ? Colors.indigoAccent : Colors.blue,
-                      );
-                    }
-                    )
-            )
-          ],
-        ));
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search, size: 30),
+                  onPressed: () {
+                    // Add search functionality
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.filter_list, size: 30),
+                  onPressed: () {
+                    // Add filter functionality
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.settings, size: 30),
+                  onPressed: () {
+                    // Add settings functionality
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

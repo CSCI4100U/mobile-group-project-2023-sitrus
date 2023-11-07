0b6e54ee1fbd1dfb03235c937c60c4a6b148d571
Nirujan Velvarathan <50958177+nirujan-v@users.noreply.github.com>
2023-11-07 15:17:22 -0500

    Update login_page.dart

--- student_helper_app/lib/pages/login_page.dart
+++ student_helper_app/lib/pages/login_page.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -15,67 +14,73 @@ class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
 
-    return Form(
-      child: Material(
-        key: formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              style: const TextStyle(fontSize: 30),
-              decoration: const InputDecoration(
-                label: Text("E-mail: "),
-                hintText: "name@email.com",
-              ),
-              validator: (value){
-                print("validating email: $value");
-                if(value!.length < 7){
-                  return "email must not be short";
-                }
-                return null;
-              },
-              onSaved: (value){
-                print("Saving Email $value");
-              },
+            key: formKey,
+            child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: [
+
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 35),
+                  child: TextFormField(
+                    style: const TextStyle(fontSize: 40, ),
+                    decoration: const InputDecoration(
+                      label: Text("Username: ", textScaleFactor: 2),
+                      hintText: "username",
+                    ),
+                    validator: (value){
+                      print("validating email: $value");
+                      if(value!.length < 7){
+                        return "email must not be short";
+                      }
+                      return null;
+                    },
+                    onSaved: (value){
+                      print("Saving username $value");
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 35),
+                  child: TextFormField(
+                    style: const TextStyle(fontSize: 30),
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      label: Text("Password: ", textScaleFactor: 2),
+
+                    ),
+                    onSaved: (value){
+                      print("Saving password $value");
+                    },
+                  ),
+                ),
+
+                ElevatedButton(
+
+                  onPressed: (){
+                  print("Attempting to login");
+
+                  if(formKey.currentState!.validate()){
+                    print("validated");
+                    formKey.currentState!.save();
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("User Logged In")));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Incorrect username or password")));
+                  }
+                }, child: const Text("Login", textScaleFactor: 1.5, style: TextStyle(fontSize: 30),  ),
+
+                )
+              ],
             ),
-            TextFormField(
-              style: const TextStyle(fontSize: 30),
-              obscureText: true,
-              decoration: const InputDecoration(
-                label: Text("Password: "),
 
-              ),
-              onSaved: (value){
-                print("Saving Email $value");
-              },
-            ),
-            DropdownButtonFormField(
-                style: const TextStyle(fontSize: 30),
-                items: <String>["Canada", "USA", "Mars"]
-                    .map<DropdownMenuItem>((String value){
-                  return DropdownMenuItem<String>(value: value,child: Text(value));
-                }).toList(),
-                onChanged: (value){
-                  print(value);
-                }),
-            ElevatedButton(onPressed: (){
-              print("Attempting to register");
 
-              if(formKey.currentState!.validate()){
-                print("validated");
-                formKey.currentState!.save();
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Registered User")));
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Registration Failed")));
-              }
-            }, child: const Text("Register", textScaleFactor: 2, style: TextStyle(fontSize: 30),),
+      ),
+    );
 
-            )
-          ],
-        ),
-      )
-
-      );
 
   }
 }

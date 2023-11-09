83c5eea1cc9d07f4a6e923bc6abb2910ee4c8418
Bridget Green <90144034+Bridget-Green@users.noreply.github.com>
2023-11-09 13:06:55 -0500

    Update and rename accommodations_search_page.dart to accommodations_add_page.dart

---
+++ student_helper_app/lib/pages/accommodations_add_page.dart
@@ -0,0 +1,83 @@
+//user can add their accommodations 
+
+import 'package:flutter/material.dart';
+
+//import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'models/sas_model/Accommodation.dart';
+import 'models/sas_model/Sas_ModelSQLite.dart';
+import 'accommodations_list_page.dart';
+
+class AddAccommodation extends StatefulWidget {
+  AddAccommodation();
+
+  @override
+  AddAccommodation_State createState() => AddAccommodation_State();
+
+
+}
+class AddAccommodation_State extends State<AddAccommodation> {
+  String selectedOption = "Not Available";
+  List<String> Options = ["Not Available", "Deadline Extension", "Accommodated Testing", "Sign Language Interpretation (ASL)", "Extra time", "Computerized Notetaking",
+    "Alternative Testing space", "Scribe"];
+  final studentIdController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+
+            DropdownButton<String>(
+              value: selectedOption,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedOption = newValue!;
+                });
+              },
+              items: Options.map((String grade) {
+                return DropdownMenuItem<String>(
+                  value: grade,
+                  child: Text(grade),
+                );
+              }).toList(),
+            ),
+            TextField(
+              controller: studentIdController,
+              decoration: InputDecoration(labelText: 'notes'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          String n = studentIdController.text;
+          String g = selectedOption;
+          final newAcmdn = Accommodation(
+            desc: n,
+            name: g,
+          );
+
+          SASModel gradesModel=new SASModel();
+          gradesModel.insertAcmdn(newAcmdn).then((newNotes) {
+            if (newNotes != null) {
+              //Navigator.of(context).pop(newGrade);
+              Navigator.of(context).push(
+                MaterialPageRoute<List<String>>(builder: (BuildContext context) {
+                  return ViewAccommodations();
+                }),
+              );
+            }
+          });
+        },
+        child: Icon(Icons.save),
+      ),
+    );
+  }
+}
--- student_helper_app/lib/pages/accommodations_search_page.dart
+++
@@ -1,2 +0,0 @@
-//user can search accommodations... ? not sure what they're searching - are they searching
-// for a specific accommodation from their accommodation list ? but then there should just be a search bar in that page instead

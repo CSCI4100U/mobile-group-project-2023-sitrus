d311e0b306ce4d6e1e1a65c40fda5581642c2247
Bridget Green <90144034+Bridget-Green@users.noreply.github.com>
2023-11-09 13:29:43 -0500

    Update accommodations_letter_email_page.dart

--- student_helper_app/lib/pages/accommodations_letter_email_page.dart
+++ student_helper_app/lib/pages/accommodations_letter_email_page.dart
@@ -1,23 +1,28 @@
-//brings up the professors for the student's courses, or displays all professors contact and allows user to search
-
-class TobeAdded extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+class Renewal_Letters extends StatefulWidget {
   @override
-  TobeAddedState createState() => TobeAddedState();
+  Renewal_LettersState createState() => Renewal_LettersState();
 }
-class TobeAddedState extends State<TobeAdded>{
+class Renewal_LettersState extends State<Renewal_Letters>{
   void _showAlertDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Popup Message"),
-          content: Text("This will contain a link to renew accommodations."),
+          content: Text("This contains a link to renew accommodations."),
           actions: [
             TextButton(
-              onPressed: () {
+              onPressed:  () async {
                 Navigator.of(context).pop();
+
+                final Uri url = Uri.parse('https://disabilityservices.ontariotechu.ca/uoitclockwork/custom/misc/home.aspx');
+                if (!await launchUrl(url)) {
+                throw Exception('Error launching page');
+                }
               },
-              child: Text("OK"),
+              child: Text("Continue?"),
             ),
           ],
         );
@@ -31,22 +36,22 @@ class TobeAddedState extends State<TobeAdded>{
         title: Text("Renew Accommodations"),
       ),
       body: Column(
-        
-        children: [
-          
-          const Text(
-            'It is important to renew your accommodations before every semester. Below is where you can renew your accomodations. Insert Instructions here.'
-        ),
 
-          ElevatedButton(
-          onPressed: () {
-            _showAlertDialog(context);
-          },
-          child: Text("Go to university site"),
-        ),
+          children: [
+
+            const Text(
+                'Insert Instructions here.'
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                _showAlertDialog(context);
+              },
+              child: Text("Go to university site"),
+            ),
 
 
-      ]),
+          ]),
     );
   }
 }

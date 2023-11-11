68fbac3982e05c51e4906a85b3bac7c7a339aab2
Bridget Green <90144034+Bridget-Green@users.noreply.github.com>
2023-11-11 17:38:00 -0500

    Update Accommodation.dart

--- student_helper_app/lib/models/sas_model/Accommodation.dart
+++ student_helper_app/lib/models/sas_model/Accommodation.dart
@@ -1,13 +1,13 @@
 class Accommodation{
   //basic layout of what information there would be per accommodation
-  Accommodation({this.id, this.name, this.desc, this.assessments});
+  Accommodation({this.id, this.name, this.desc, required this.assessments});
   int? id;
-  //Below, we have 'name' for 'what is this accommodation called 
+  //Below, we have 'name' for 'what is this accommodation called
   String? name;
   //'desc' is description
   String? desc;
   //'assessments' details what assessments get affected by this accommodation
-  List<String>? assessments;
+  List<String> assessments = [''];
   Map<String, Object?> toMap() {
     return {
       'id': id,

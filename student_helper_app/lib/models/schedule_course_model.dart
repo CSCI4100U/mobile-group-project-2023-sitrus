a1bf7b95f695a7d8edac518edb5549c4cf141021
Evan Dong <evandong777@gmail.com>
2023-11-12 17:54:53 -0500

    finished implementing my local project into the main, still have to change some things in home page to have it link to schedules

--- student_helper_app/lib/models/schedule_course_model.dart
+++ student_helper_app/lib/models/schedule_course_model.dart
@@ -1,21 +1,45 @@
-//course.dart
+import 'package:flutter/material.dart';
+
 class Course {
   String? className;
-  String? dayOfWeek;
   List<ClassTime>? times;
+  Color? color;
 
-  Course(this.className, this.times);
-  //to get each class time, you can loop through this list and get the start/end time for each class
-  //e.g. for (i = 0 to length) {starttime = times[i].startTime endtime = times[i].endTime}
-  //or something like: for classTime in times {startTime = classTime.startTime endTime = classTime.endTime}
+  Course(this.className, this.times, this.color);
 
+  //maybe have a setColor function?
 }
 
 class ClassTime {
   HourMinute? startTime;
   HourMinute? endTime;
+  String? dayOfWeek;
 
-  ClassTime(this.startTime, this.endTime);
+  ClassTime(this.startTime, this.endTime, this.dayOfWeek);
+
+  int lengthOfClass() {
+    return this.endTime!.convertToInt() - this.startTime!.convertToInt();
+  }
+
+  int getDayOfWeekAsInt() {
+    //maybe use switch statements? idk if it's necessarily faster/better
+
+    if (dayOfWeek == "Sunday") {
+      return 0;
+    } else if (dayOfWeek == "Monday") {
+      return 1;
+    } else if (dayOfWeek == "Tuesday") {
+      return 2;
+    } else if (dayOfWeek == "Wednesday") {
+      return 3;
+    } else if (dayOfWeek == "Thursday") {
+      return 4;
+    } else if (dayOfWeek == "Friday") {
+      return 5;
+    } else {  //Saturday
+      return 6;
+    }
+  }
 
 }
 
@@ -26,6 +50,6 @@ class HourMinute {
   HourMinute(this.hour, this.minute);
 
   int convertToInt() {
-    return (this.hour! * 1000) + (this.minute!);
+    return (this.hour! * 100) + (this.minute!);
   }
 }
--- student_helper_app/lib/models/schedule_schedule_model.dart
+++ student_helper_app/lib/models/schedule_schedule_model.dart
@@ -1,10 +1,96 @@
 import 'schedule_course_model.dart';
-//schedule.dart
+
+//for demo, it is fixed to 5 courses in a schedule object (i.e. courses list is length 5)
+
 class Schedule {
   List<Course>? courses;
 
-  //containsCourse - used to check if the schedule contains a course with the given className.
-  //plan to be used for searching schedules by class name
+  Schedule(this.courses);
+
+  static List<Schedule> validateSchedules(List<List<Course>> schedules) {
+    List<Schedule> validatedSchedules = [];
+
+    for (List<Course> schedule in schedules) {
+      if (!conflictExists(schedule)) {
+        validatedSchedules.add(Schedule(schedule));
+      }
+    }
+    return validatedSchedules;
+  }
+
+  static bool conflictExists(List<Course> schedule) {
+    int numOfCourses = schedule.length;
+    //to check for conflict between every course in the schedule
+    //iterate through each course in the list
+    for (int currentCourseIndex = 0; currentCourseIndex < numOfCourses; currentCourseIndex++) {
+      //for each course, compare it with every other course in the list by iterating
+      // through each course in the list starting after the current course
+      for (int courseBeingComparedIndex = currentCourseIndex + 1; courseBeingComparedIndex < numOfCourses; courseBeingComparedIndex++) {
+        //to check if the current course conflicts with the course being compared to, must check
+        // for conflict between the class times for both courses
+        //to do this, iterate through each class's times in the list of class's times for current course
+        for (ClassTime classTimeCurrentCourse
+        in schedule[currentCourseIndex].times!) {
+          //then iterate through each class's times in the list of class's time for course being compared to
+          for (ClassTime classTimeCourseBeingCompared
+          in schedule[courseBeingComparedIndex].times!) {
+            //check for conflict between the two class's times by seeing if they intersect each other
+
+            //if the classes are on different days, they don't intersect
+            if (classTimeCurrentCourse.dayOfWeek !=
+                classTimeCourseBeingCompared.dayOfWeek) {
+              break;
+            }
+            //if classes are on same day, check for conflict
+            //class times intersect if either course's start/end time is in between the other course's time
+            //if statement 1 (splitting because the comparisons are too long...:
+            // i.e. (currentCourse.starTime < courseBeingCompared.startTime < currentCourse.endTime) or
+            //      (courseBeingComparedTo.startTime < currentCourse.endTime < courseBeingComparedTo.endTime)
+            //visual representation:
+            // currentCourse: start|------------|end
+            // courseBeingCompared    start|------------|end
+            if ((classTimeCurrentCourse.startTime!.convertToInt() <
+                  classTimeCourseBeingCompared.startTime!.convertToInt()
+                  &&
+                  classTimeCourseBeingCompared.startTime!.convertToInt() <
+                  classTimeCurrentCourse.endTime!.convertToInt())
+                ||
+                (classTimeCourseBeingCompared.startTime!.convertToInt() <
+                  classTimeCurrentCourse.endTime!.convertToInt()
+                  &&
+                  classTimeCurrentCourse.endTime!.convertToInt() <
+                  classTimeCourseBeingCompared.endTime!.convertToInt())) {
+              return true;
+            }
+            //if statement 2:
+            // i.e (currentCourse.startTime < courseBeingComparedTo.endTime < currentCourse.endTime) or
+            //      (courseBeingComparedTo.startTime < currentCourse.startTime < courseBeingComparedTo.endTime)
+            //visual representation
+            // currentCourse:             start|------------|end
+            // courseBeingCompared  start|------------|end
+            if ((classTimeCurrentCourse.startTime!.convertToInt() <
+                  classTimeCourseBeingCompared.endTime!.convertToInt()
+                  &&
+                  classTimeCourseBeingCompared.endTime!.convertToInt() <
+                  classTimeCurrentCourse.endTime!.convertToInt())
+                ||
+                (classTimeCourseBeingCompared.startTime!.convertToInt() <
+                  classTimeCurrentCourse.startTime!.convertToInt()
+                  &&
+                  classTimeCurrentCourse.startTime!.convertToInt() <
+                  classTimeCourseBeingCompared.endTime!.convertToInt())) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    //if no class times for the courses intersect, there are no conflicts, so return false
+    return false;
+  }
+
+//containsCourse - used to check if the schedule contains a course with the given className.
+  //plan to be used in future for searching schedules by class name
   bool containsCourse(String className) {
     //or maybe pass it a Course object? and do if paramCourse.clasName = course.className
     for (Course course in courses!) {
@@ -14,26 +100,5 @@ class Schedule {
     }
     return false;
   }
-  //maybe have a function that returns a map ??
-
-  //don't worry about below... my brain stopped working or something and i started typing stuff
-  //courses list is:
-  // [course1, course2]
-  //expands to a list of Course, each with className, and times list
-  //[
-  //  [className1, times],
-  //  [className1, times],
-  //]
-  //
-  //[
-  //  [className1, [
-  //    classTime1, classTime2
-  //    ]
-  //  ],
-  //  [className1, [
-  //    classTime1, classTime2
-  //    ]
-  //  ],
-  //]
 
 }

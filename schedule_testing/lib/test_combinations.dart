beae032c774d21d4072e6f2880ca9dbb0421c046
Evan Dong <evandong777@gmail.com>
2023-11-28 01:39:30 -0500

    able to make all combinations for the sections of a course, still need to finish up combinations for tuts and combinations for labs, but those should be very similar to section combinations

--- schedule_testing/lib/test_combinations.dart
+++ schedule_testing/lib/test_combinations.dart
@@ -46,16 +46,18 @@ void main() {
   Section pl_sec2 = Section([tue8_9]);
   Section prob_sec1 = Section([wed8_9, fri8_9]);
   Section md_sec1 = Section([wed9_11, fri9_11]);
-  Section md_sec2 = Section([wed2_3, fri2_3]);
+  // Section md_sec2 = Section([wed2_3, fri2_3]);
+  Section md_sec2 = Section([tue2_3, thu2_3]);
   Section ada_sec1 = Section([mon11_12, thu11_12]);
-  Section ada_sec2 = Section([mon2_3, thu2_3]);
+  // Section ada_sec2 = Section([mon2_3, thu2_3]);
+  Section ada_sec2 = Section([tue2_3, thu2_3]);
   Section eth_sec1 = Section([wed11_12]);
   Section d_sec1 = Section([mon9_11, tue9_11]);
 
   List<Section> pl_secs = [pl_sec1, pl_sec2];
   List<Section> prob_secs = [prob_sec1];
   List<Section> md_secs = [md_sec1, md_sec2];
-  List<Section> ada_secs = [ada_sec1, ada_sec2];
+  List<Section> ada_secs = [ada_sec1];
   List<Section> eth_secs = [eth_sec1];
   List<Section> d_secs = [d_sec1];
 
@@ -75,7 +77,7 @@ void main() {
     md,
     ada,
     eth,
-    // d,
+    d,
   ];
 
   int? numOfCoursesInSchedule = 5; //this will be taken from user input like; however, doing hard coded 5
@@ -84,13 +86,33 @@ void main() {
   Combinations comb = Combinations(coursesFromInput: coursesFromInput);
 
   comb.getAllCourseCombinations(coursesFromInput, numOfCoursesInSchedule, []);
-  for (List<Course> schedule in comb.schedules!) {
+  // for (List<Course> schedule in comb.schedules!) {
+  //   for (Course course in schedule) {
+  //     print(course.courseName);
+  //   }
+  //   print('----schedule---');
+  // }
+  //expected output from above (with comb.schedules = [pl, prob, md, ada, eth]:
+  //'pl' 'prob' 'md' 'ada' 'eth'
+
+  for (List<Course> schedule in comb.schedules) {
+    comb.getAllOneSectionCombinations(schedule);
+  }
+  for (List<Course> schedule in comb.schedulesOneSection) {
     for (Course course in schedule) {
-      print(course.courseName);
+      print('${course.courseName}, ${course.sections[0].lectureTimes?[0].dayOfWeek}');
     }
     print('----schedule---');
   }
-
+  //expected output from above (with comb.schedules = [pl, prob, md, ada, eth]:
+  //c1.sec[0],   c2.sec[0],        c3.sec[0],      c4.sec[0],    c5.sec[0]
+  //1 pl monday, 2 prob wednesday, 3 md wednesday, 4 ada monday, 5 eth wednesday
+  //c1.sec[0],   c2.sec[0],        c3.sec[1],    c4.sec[0],  c5.sec[0]
+  //1 pl monday, 2 prob wednesday, 3 md tuesday, 4 ada monday, 5 eth wednesday
+  //c1.sec[1],    c2.sec[0],        c3.sec[0],      c4.sec[0],    c5.sec[0]
+  //1 pl tuesday, 2 prob wednesday, 3 md wednesday, 4 ada monday, 5 eth wednesday
+  //c1.sec[1],    c2.sec[0],        c3.sec[1],    c4.sec[0],    c5.sec[0]
+  //1 pl tuesday, 2 prob wednesday, 3 md tuesday, 4 ada monday, 5 eth wednesday
 
 
 }
@@ -101,10 +123,10 @@ class Combinations {
   Combinations({required this.coursesFromInput});
 
   //'re-initialize' this
-  List<List<Course>>? schedules = [];
-  List<List<Course>>? schedulesOneSection = [];
-  List<List<Course>>? schedulesOneSectionOneTut = [];
-  List<List<Course>>? schedulesOneSectionOneTutOneLab = [];
+  List<List<Course>> schedules = [];
+  List<List<Course>> schedulesOneSection = [];
+  List<List<Course>> schedulesOneSectionOneTut = [];
+  List<List<Course>> schedulesOneSectionOneTutOneLab = [];
 
   //given a list of courses, create all combinations of k (e.g. 5) courses, (n Choose k) n = coursesFromInput.length k = numOfCoursesInSchedule
   void getAllCourseCombinations(List<Course> courses, int numOfCoursesInSchedule, List<Course> currentCombination) {
@@ -122,99 +144,48 @@ class Combinations {
 
   //want to create new schedule with 11111, then 21111, then 12111, then 22111, then 11211, and so on
   // 1 represents sec 1, 2 represents sec 2. place of digit represents
+  //given a list of courses that have been 'reduced' to length k (e.g. 5), k = numOfCoursesInSchedule from above
+  //  create all the combinations of k courses with the different sections
+  //  e.g. pass List<Course> = [c1, c2, c3, c4, c5]. for each section, create new List<Course> (1st iteration) = [c1.sec1, c2.sec1, c3.sec1, c4.sec1, c5.sec1]
+  //                                                   (2nd iteration) = [c1.sec2, c2.sec1, c3.sec1, c4.sec1, c5.sec1]
+  void getAllOneSectionCombinations(List<Course> schedule) {
 
-  List<List<int>> generateCombinations(List<int> maxSizes) {
-    List<List<int>> combinations = [];
+    List<int> sectionNumbers = [];
+    sectionNumbers = List.filled(schedule.length, 0);
 
-    void generate(List<int> currentCombination, int currentIndex) {
-      if (currentIndex == maxSizes.length) {
-        combinations.add(List.from(currentCombination));
+    List<Course> currentSchedule = [];
+
+    void generateSectionCombinations(List<Course> currentSchedule, int currentIndex) {
+      if (currentIndex == schedule.length) {
+        int currentCourse = 0;
+        for (Course course in schedule) {
+          currentSchedule.add(Course(course.courseName, [course.sections[sectionNumbers[currentCourse]]], course.tutorials, course.labs, course.color));
+          currentCourse++;
+        }
+        schedulesOneSection.add(currentSchedule);
         return;
       }
 
-      for (int i = 0; i <= maxSizes[currentIndex]; i++) {
-        currentCombination[currentIndex] = i;
-        generate(currentCombination, currentIndex + 1);
+      for (int sectionNumber = 0; sectionNumber < schedule[currentIndex].sections.length; sectionNumber++) {
+        currentSchedule = [];
+        sectionNumbers[currentIndex] = sectionNumber;
+        generateSectionCombinations(currentSchedule, currentIndex + 1);
       }
     }
 
-    generate(List.filled(maxSizes.length, 0), 0);
-    return combinations;
+    generateSectionCombinations(currentSchedule, 0);
   }
 
-  void getAllSectionCombinations(List<Course> courses) {
-    //given a list of courses that have been 'reduced' to length k (e.g. 5), k = numOfCoursesInSchedule from above
-    //  create all the combinations of k courses with the different sections
-    //  e.g. pass List<Course> = [c1, c2, c3, c4, c5]. for each section, create new List<Course> (1st iteration) = [c1.sec1, c2.sec1, c3.sec1, c4.sec1, c5.sec1]
-    //                                                   (2nd iteration) = [c1.sec2, c2.sec1, c3.sec1, c4.sec1, c5.sec1]
-
-    List<int> sectionNumbers = [];
-    List<Course> coursesButOnlyOneSection = [];
-
-    for (Course course in courses) {
-      sectionNumbers.add(0);
-    }
-
-    int upperPosition = 0;
-    int lowerPosition = 0;
-    coursesButOnlyOneSection = [];
-    int sectionCount = 0;
-
-
-      //start at course 0, go through every course and add the first section
-      //for Course course in courses, add Course(course.courseName, course.sections[sectionNumbers[currentPosition], course.tutorials, course.labs, course.color)
-
-      //increment course 0's sectionNumber
-
-    //reset courseButOnlyOneSection
-    //add each course but with only one section:
-    //  for int i = 0; i < courses.length:
-    //    courseButOnlyOneSection.add(Course(courses[i].courseName, courses[i].sections[sectionNumbers[i], courses[i].tutorials, courses[i].labs, courses[i].color))
-    //    schedulesOneSection.add(coursesButOnlyOneSection)
-    //check if there are more sections for the currentPosition course; sectionNumber[currentPosition] < courses[currentPosition].sections.length, if yes:
-    //  increment sectionNumber[currentPosition]
-    //if no, no more sections for current course, move current to next course:
-    //  increment currentPosition
-    //  check if we reached 'most significant' digit, want to increment it and go back to loop to it again. currentPosition >= upperPosition, if yes:
-    //    MUST CHECK IF ALL PREVIOUS POSITIONS HAVE MAX SECTION NUMBERS, IF NO, KEEP UPPER POSITION AND ITS SECTION NUMBER SAME, GO BACK AND INCREMENT FROM LEAST
-    //      IF YES THEN CAN PROCEED WITH INCREMENTING UPPER POSITION AND RESETTING PREVIOUS SECTION NUMBERS (l167)
-    //    reset currentPosition to start; currentPosition = 0
-    //    check if there are more sections for the upperPosition course; sectionNumber[upperPosition] < courses[upperPosition].sections.length, if yes:
-    //      increment sectionNumber[upperPosition]
-    //      reset sectionNumbers before upperPosition, sectionNumbers[< upperPosition] = 0
-    //    if no, go to next course and make combinations with all its sections:
-    //      reset sectionNumbers
-    //      increment upperPosition
-    //      increment sectionNumbers[upperPosition]
-
-    // for (int upperPosition = 0; upperPosition < courses.length; upperPosition++) {  //0, 1, 2, 3, 4
-      // for (int currentPosition = 0; currentPosition < courses.length; currentPosition++) {  //0, 1, 2, 3, 4
-      //   coursesButOnlyOneSection.add(Course(
-      //       courses[currentPosition].courseName,
-      //       [courses[currentPosition].sections[sectionNumbers[currentPosition]]],
-      //       courses[currentPosition].tutorials,
-      //       courses[currentPosition].labs,
-      //       courses[currentPosition].color));
-      // }
-      //for Course course in courses, add Course(course.courseName, course.sections[sectionNumbers[currentPosition], course.tutorials, course.labs, course.color)
-    // }
-    //make an one section schedule
-    // for (Course course in courses) {
-    //   coursesButOnlyOneSection.add(Course(course.courseName, course.sections, course.tutorials, course.labs, course.color));
-    // }
+  void getAllOneSectionOneTutorialCombinations(List<Course> schedule) {
 
   }
 
-  //for schedule in schedules
-  //  for courses in schedule
-  //
-  //for each course in courses? ... List<Course> new_courses.add(Course(courses[i].courseName, courses[i].sections[ints[i]], courses[i].tutorials, courses[i].labs))
-  //schedulesbutwithonlyonesection.add(Schedule(new_courses))
+  void getAllOneSectionOneTutorialOneLabCombinations(List<Course> schedule) {
+
+  }
+
+
   //check for conflict between lectures - need to modify current conflict check a little. i.e classtimes -> sections.lecturetimes
   //get new schedules with no lecture conflicts
 
-  //for each schedule in schedules
-  //  for courses in schedule
-  //    for course in courses
-  //      List
 }

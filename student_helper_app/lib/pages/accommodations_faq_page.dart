848b83cab01b53e02b8da15096d2d2623bc3e064
Bridget Green <90144034+Bridget-Green@users.noreply.github.com>
2023-12-01 09:52:02 -0500

    Update accommodations_faq_page.dart

--- student_helper_app/lib/pages/accommodations_faq_page.dart
+++ student_helper_app/lib/pages/accommodations_faq_page.dart
@@ -15,35 +15,38 @@ class FAQPageState extends State<FAQPage> {
   final List<FAQItem> faqList = [
     FAQItem(
       question: 'What are accommodations?',
-      answer: 'Accommodations are services provided to students with disabilities/handicaps that prevent them'
-          'from working at the same level as their peers.',
+      answerSections:[ 'Accommodations are services provided to students with disabilities/handicaps that prevent them'
+          'from working at the same level as their peers.',]
     ),
     FAQItem(
       question: 'Do I have accommodations?',
-      answer: 'If you are registered with your universitys student accessibility services '
-          'it is very likely that you have accommodations.',
+      answerSections: ['If you are registered with your university\'s student accessibility services, '
+          'it is very likely that you have accommodations.',]
     ),
     FAQItem(
       question: 'What is this for?',
-      answer: 'This part of the app is meant to make the process of managing your accommodations easier.'
+      answerSections: ['This part of the app is meant to make the process of managing your accommodations easier.'
           'Navigating Student Accessibility can be complicated, especially for those unfamiliar with it.'
           'Here, you can see how your accommodations affect the way you take assignments.'
           'You can also schedule your upcoming assignments, to see what your accommodations provide you with.',
-
+]
     ),
     FAQItem(
       question: 'How can I register for Student Accessibility?',
-      answer: 'This is a process that requires a few steps, and is encouraged to be done as soon as possible.\n\nStep 1: You need to have recent documentation from a certified health care practitioner in the field to which the disability applies, '
-          '\tand these must include the functional or cognitive limitations and impact on the student’s academic performance.\nYou may need'
-          'to arrange an appointment to be reassessed to have more recent documentation.\n\n Step 2: Complete an intake form. This can be difficult to locate on UOITs student portal.'
-          '\tFor this reason, the intake form has been included in Renew Accommodations.\n\n Step 3:'
-          'You will need to complete a disability documentation form. This has been provided in Renew Accommodations.\n\nStep 4:'
-          'Submit the intake and disability documents by emailing studentaccessibility@ontariotechu.ca. \n\nStep 5:'
-          'Wait for an email response from  studentaccessibility@ontariotechu.ca.\n They will help you arrange'
-          'an appointment with an accessibility advisor.'
-      ,
-    )
+      answerSections: [
+        'This is a process that requires a few steps and is encouraged to be done as soon as possible.',
+        'Step 1: You need to have recent documentation from a certified health care practitioner in the field to which the disability applies, '
+            'and these must include the functional or cognitive limitations and impact on the student’s academic performance.',
+        'You may need to arrange an appointment to be reassessed to have more recent documentation.',
+        'Step 2: Complete an intake form.',
+        'This can be difficult to locate on UOIT\'s student portal. For this reason, the intake form has been included in Renew Accommodations.',
+        'Step 3: You will need to complete a disability documentation form. This has been provided in Renew Accommodations.',
+        'Step 4: Submit the intake and disability documents by emailing studentaccessibility@ontariotechu.ca.',
+        'Step 5: Wait for an email response from studentaccessibility@ontariotechu.ca. They will help you arrange an appointment with an accessibility advisor.',
+      ],
+    ),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,15 +59,24 @@ class FAQPageState extends State<FAQPage> {
           return ExpansionTile(
             title: Text(faqList[index].question),
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  faqList[index].answer,
-                  softWrap: true,
+              Container(
+                height: 200, // Set a specific height or use constraints based on your design
+                child: ListView.builder(
+                  itemCount: faqList[index].answerSections.length,
+                  itemBuilder: (context, sectionIndex) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        faqList[index].answerSections[sectionIndex],
+                        softWrap: true,
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
           );
+
         },
       ),
     );
@@ -73,7 +85,35 @@ class FAQPageState extends State<FAQPage> {
 
 class FAQItem {
   final String question;
-  final String answer;
+  //final String answer;
+  Widget getFormattedAnswer() {
+    return ListView.builder(
+      itemCount: answerSections.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                // Check if the section starts with 'Step 1:' and make it bold
+                answerSections[index].startsWith('Step')
+                    ? TextSpan(
+                  text: answerSections[index],
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )
+                    : TextSpan(text: answerSections[index]),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
-  FAQItem({required this.question, required this.answer});
+  final List<String> answerSections;
+
+  FAQItem({
+    required this.question,
+    required this.answerSections,
+  });
 }
